3372ca27f1130078c9547ddd706b8dc3ae9d01e7
koudai0711 <forestman_0711@docomo.ne.jp>
2020-03-25 15:15:17 +0900

    コミットログイン機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :tweet_params, only: [:create]
-  
+  before_action :move_to_index, except: [:index]
   def index
     @tweets = Tweet.all
   end
@@ -16,5 +16,9 @@ class TweetsController < ApplicationController
   private
   def tweet_params
     params.require(:tweet).permit(:name, :text, :image)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
