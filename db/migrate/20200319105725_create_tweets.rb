3d47359f08aa0ea415194d61848852c33fb65183
koudai0711 <forestman_0711@docomo.ne.jp>
2020-05-01 20:36:41 +0900

    [WIP]

--- db/migrate/20200319105725_create_tweets.rb
+++ db/migrate/20200319105725_create_tweets.rb
@@ -1,9 +1,14 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-      t.string :name
-      t.string :text
-      t.text :image
+      t.string     :name,           null: false
+      t.string     :text            null: false
+      t.string     :status,         null: false
+      t.string     :size,           null: false
+      t.integer    :prefecture_id,  null: false
+      t.string     :buyer_id
+      t.string     :saler_id
+      t.references :category,       null: false
       t.timestamps
     end
   end
